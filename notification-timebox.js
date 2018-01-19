2bd2a56334b74a22f5d958ef457fe330a84a0570
Marcus <marcus_amaral@msn.com>
2018-01-19 11:54:28 -0200

    Versão 1.4 com configuração de permissão de áudio e opção de hora de saída alternativa.

--- notification-timebox.js
+++ notification-timebox.js
@@ -1,10 +1,42 @@
-let notificouSaida = { jaNotificou: document.querySelector("#HoraSaida") ? true : false };
-let notificouAlmoco = { jaNotificou: document.querySelector("#hdfHoraEntradaAlmoco") ? true : false };
 const audio = new Audio('https://freesound.org/data/previews/109/109662_945474-lq.mp3');
 const icone = 'https://cdn1.iconfinder.com/data/icons/web-essentials-circle-style/48/clock-2-512.png';
+
+let notificouSaida = { jaNotificou: document.querySelector("#HoraSaida") ? true : false };
+let notificouAlmoco = { jaNotificou: document.querySelector("#hdfHoraEntradaAlmoco") ? true : false };
+let audioPermission = false;
+let twominutes = false;
+let newHour = null;
+
+chrome.storage.sync.get('audioPermission', response => {
+    audioPermission = response.audioPermission;
+});
+chrome.storage.sync.get('twominutes', response => {
+    twominutes = response.twominutes;
+});
+chrome.storage.sync.get('newHour', response => {
+    newHour = response.newHour;
+});
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (key in changes) {
+        let storageChange = changes[key];
+        switch (key) {
+            case 'audioPermission':
+                audioPermission = storageChange.newValue;
+                break;
+            case 'twominutes':
+                twominutes = storageChange.newValue;
+                break;
+            case 'newHour':
+                newHour = storageChange.newValue;
+                break;
+        }
+    }
+});
+
 setInterval(function () {
-    let newDate = new Date();
-	let atual = newDate.getHours() + ':' + newDate.getMinutes();
+    let horaAtual = new Date();
+	let atual = horaAtual.getHours() + ':' + horaAtual.getMinutes();
     let saida = document.querySelector("#HoraPrevistaSaida").value ? document.querySelector("#HoraPrevistaSaida").value.split(' ')[1].split(':')[0] + ':' + document.querySelector("#HoraPrevistaSaida").value.split(' ')[1].split(':')[1] : '';
     let almoco = '';
 
@@ -21,7 +53,7 @@ setInterval(function () {
       });
     }
 
-    if (document.querySelector("#hdfHoraSaidaAlmoco").value) {
+    if (document.querySelector("#hdfHoraSaidaAlmoco") && document.querySelector("#hdfHoraSaidaAlmoco").value) {
         let almocoHora = parseInt(document.querySelector("#hdfHoraSaidaAlmoco").value.split(':')[0]);
         almocoHora++;
         almoco = almocoHora.toString() + ':' + document.querySelector("#hdfHoraSaidaAlmoco").value.split(':')[1];
@@ -34,7 +66,7 @@ setInterval(function () {
         }
     });
 
-    verirficaHora(saida, atual, notificouSaida, {titulo: "Hora de bater o ponto!", corpo: { 
+    verirficaHora(newHour ? newHour : saida, atual, notificouSaida, {titulo: "Hora de bater o ponto!", corpo: { 
             body: 'Lembre-se de ir embora.',
             tag: 'saida',
             icon: icone
@@ -55,6 +87,8 @@ function verirficaHora(prevista, atual, notificou, notificacao) {
 function mostraNotificacao(notificacao) {
     if (Notification.permission === "granted") {
         let notification = new Notification(notificacao.titulo, notificacao.corpo);
-        audio.play();
+        if (audioPermission) {
+            audio.play();
+        }
     }
 }
--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
 let promises = [];
 
-promises.push(chrome.storage.sync.get('twominutes', response => {
-    if (response.twominutes) {
-        document.getElementById('twominutes').checked = true;
-    } else {
-        document.getElementById('twominutes').checked = false;
-    }
-}));
+// promises.push(chrome.storage.sync.get('twominutes', response => {
+//     if (response.twominutes) {
+//         document.getElementById('twominutes').checked = true;
+//     } else {
+//         document.getElementById('twominutes').checked = false;
+//     }
+// }));
 
 promises.push(chrome.storage.sync.get('audioPermission', response => {
     if (response.audioPermission) {
@@ -16,23 +16,61 @@ promises.push(chrome.storage.sync.get('audioPermission', response => {
     }
 }));
 
+promises.push(chrome.storage.sync.get('newHour', response => {
+    if (response.newHour) {
+        document.getElementById('overwriteHour').checked = true;
+        document.getElementById('hourOverwited').disabled = false;
+        document.getElementById('hourOverwited').value = response.newHour;
+    } else {
+        document.getElementById('overwriteHour').checked = false;
+        document.getElementById('hourOverwited').disabled = true;
+    }
+}));
+
 Promise.all(promises).then((resp) => {
     document.getElementById('body').style = 'display: block';
 });
 
+/* FUNÇÃO PARA SALVAR O AVISO DE 2 MINUTOS */
+// document.getElementById('twominutes').addEventListener('change', saveTwoMinutes);
 
-document.getElementById('twominutes').addEventListener('change', saveTwoMinutes);
+// function saveTwoMinutes(e) {
+//     chrome.storage.sync.set({'twominutes': e.target.checked}, function() {
+//         console.log('Two Minutes ' + (e.target.checked ? 'Ativado' : 'Desativado'));
+//     });
+// }
 
-function saveTwoMinutes(e) {
-    chrome.storage.sync.set({'twominutes': e.target.checked}, function() {
-        console.log('Two Minutes ' + (e.target.checked ? 'Ativado' : 'Desativado'));
-    });
-}
-
+/* FUNÇÃO PARA SALVAR A PERMISSÃO DE ÁUDIO */
 document.getElementById('audioPermission').addEventListener('change', saveAudioPermission);
 
 function saveAudioPermission(e) {
     chrome.storage.sync.set({'audioPermission': e.target.checked}, function() {
         console.log('Áudio ' + (e.target.checked ? 'Ativado' : 'Desativado'));
     });
+}
+
+/* FUNÇÃO PARA SALVAR QUE O USUÁRIO DESEJA SOBRESCREVER A HORA AUTOMÁTICA */
+document.getElementById('overwriteHour').addEventListener('change', toggleOverwriteHour);
+document.getElementById('hourOverwited').addEventListener('change', overwriteHour);
+
+function toggleOverwriteHour(e) {
+    if (e.target.checked) {
+        document.getElementById('hourOverwited').disabled = false;
+    } else {
+        document.getElementById('hourOverwited').disabled = true;
+        document.getElementById('hourOverwited').value = null;
+    }
+}
+
+function overwriteHour(e) {
+    if (document.getElementById('overwriteHour')) {
+        chrome.storage.sync.set({'newHour': document.getElementById('hourOverwited').value}, function() {
+            console.log('Notificação da hora de saída sobrescrita para ' + document.getElementById('hourOverwited').value);
+        });
+    } else {
+        chrome.storage.sync.set({'newHour': null}, function() {
+            console.log('Notificação da hora de saída voltou para o padrão');
+        });
+    }
+    
 }
